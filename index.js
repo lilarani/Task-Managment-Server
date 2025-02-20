244877c2f6dfe77dcd0806d9cf583c09824819de
neelarani <neela9624@gmail.com>
2025-02-21 00:15:22 +0600

    Add POST API to store user info after Google sign-in

---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdepx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db('TaskFlow');
+    const userCollection = database.collection('users');
+
+    // user post
+    app.post('/userInfo', async (req, res) => {
+      const info = req.body;
+      const result = await userCollection.insertOne(info);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('TaskFlow is running');
+});
+
+app.listen(port, () => {
+  console.log(`TaskFlow port is: ${port}`);
+});
